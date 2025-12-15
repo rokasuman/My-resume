660b89ef194cc63c80927f667c31b0705b156bca
rokasuman <roka7362@gmail.com>
2025-12-15 13:37:15 +1100

    bio upadated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,20 +8,25 @@ const About = () => {
       </div>
       <div className="about-sections">
         <p>
-          Hi, I am <strong>Suman Roka</strong>, an avid software Developer currently studing IT
-          in Sydney with strong fundation in Computer Science.My expertise lies
-          in web development using MERN stack. However, as my major is AI, i am
-          equally enthusatic about Machine Learning and Blockchain Technologies.
-          I intent to dive deeper into web developmnet and constantly strive to
+          Hi, I am <strong>Suman Roka</strong>, an avid software Developer
+          currently studing IT in Sydney with strong fundation in Computer
+          Science.I am developing my skills in web development with the MERN
+          stack, while pursuing a major in Artificial Intelligence. I am equally
+          passionate about Machine Learning and Blockchain technologies. I
+          intent to dive deeper into web developmnet and constantly strive to
           intergrate modern technologies into the Web development to enhnace the
           user experince, improvce the performace and ensure reliability.
         </p>
-        <p>I am looking Forward to connect with fellow developers and industry professional to exchange idea, share knowledge and embark onan incredile journey of growth and innovation</p>
+        <p>
+          I am looking Forward to connect with fellow developers and industry
+          professional to exchange idea, share knowledge and embark onan
+          incredile journey of growth and innovation
+        </p>
       </div>
       <div className="education">
         <ul>
-            <li>SHEA: Currently Studying</li>
-            {/*<li>Dented Code Academy :Traineeship</li>*/}
+          <li>SHEA: Currently Studying</li>
+          {/*<li>Dented Code Academy :Traineeship</li>*/}
         </ul>
       </div>
     </div>
